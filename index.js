e372d9ddf490539e1a48bcb1fb04d4efe017a4b5
Philip Claren <philip@claren.io>
2020-03-10 20:21:10 +0100

    set port from env

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ io.on('connection', function (socket) {
 });
 
 
-http.listen(3000, function () {
-  console.log('listening on *:3000');
+http.listen(process.env.PORT, function () {
+  console.log(`listening on ${process.env.PORT}`);
 });
 
 class Call {
