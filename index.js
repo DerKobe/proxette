b8ed4e3862f105383c68581480d1fbb1539c1fbb
Philip Claren <philip@claren.io>
2020-03-11 08:24:35 +0100

    removed isomorphic-fetch 2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const fetch = require('isomorphic-fetch');
 const { v4: uuidv4 } = require('uuid');
 
 const calls = [];
